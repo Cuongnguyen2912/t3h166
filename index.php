127b2d0e58a8d3834a5e60ab1dcd9e851a1fc495
Cuongnguyen2912 <64520088+Cuongnguyen2912@users.noreply.github.com>
2020-06-16 20:10:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,48 @@
 
 <?php
 
-class Student
-{
+
+class Student {
+
     // gán giá trị mặc định cho thuộc tính $name
-    public $name = 'họ tên';
+    public $name = 'demo họ tên';
+
+    // thêm thuộc tính tĩnh
+    public static $location = 'việt nam';
 
 
-    public static function demo1()
-    {
+    // phương thức tĩnh
+    public static function demo1() {
         echo '<br>' . __METHOD__;
-        // không được sử dụng $this trong phương thức tĩnh
-        // nếu sử dụng thì bị lỗi
+        // không được sử dụng từ khóa $this trong phương thức tĩnh
+        // nếu sử dụng thì sẽ bị lỗi
+        echo '<br> truy cập đến phương thức thông thường từ bên trong phương thức tĩnh';
+        $self = new self();
+        // truy cập thuộc tính bình thường
+        echo '<br>' . $self->name;
+        // truy cập phương thức bình thức
+        echo '<br>' . $self->demo2();
+
+        // truy cập đến thuộc tính tĩnh
+        echo '<br> truy cập đến thuộc tính tĩnh';
+        echo '<br>' . self::$location;
+
     }
 
     // phương thức bình thường
-    public function demo2()
-    {
+    public function demo2() {
         echo '<br>' . __METHOD__;
         echo '<br>' . $this->name;
     }
+
 }
-  // khởi tạo đối tượng từ class
+
+// khởi tạo đối tượng từ class
 $tuan = new Student();
-$tuan -> demo2();
-// ưu điểm có thể gọi đến màn không
-// cần khởi tạo đối tượng
-// cách gọi Tenclass::Tenphuongthuctinh
+// muốn gọi đến phương thức bình thường
+$tuan->demo2();
+
+// phương thức tĩnh có ưu điểm là có thể gọi đến phương thức tĩnh màn không
+// cần phải khởi tạo đối tượng
+// cách gọi TenClass::TenPhuongthucTinh
 Student::demo1();
