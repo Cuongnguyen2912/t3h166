97258eb6b6dd2897efde96ad5d58f3e997ec6563
Cuongnguyen2912 <64520088+Cuongnguyen2912@users.noreply.github.com>
2020-06-16 19:51:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
-echo 'hi nqc2912 sugar';
+class Student
+{
+    // gán giá trị mặc định cho thuộc tính $name
+    public $name = 'họ tên';
+
+    public static function demo1()
+    {
+        echo '<br>' . __METHOD__;
+        // không được sử dụng $this trong phương thức tĩnh
+        // nếu sử dụng thì bị lỗi
+    }
+
+       // phương thức bình thường
+    public function $name(){
+       echo $name => $this;
+}
+
+}
